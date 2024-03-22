fded0a93645a01077c5aaedd39219508b70e1a93
Coraville <108678949+Coraville@users.noreply.github.com>
2024-03-22 17:38:57 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,32 +1,12 @@
-package lista;
-
 public class Main {
-
-	public static void main(String[] args) {
-		
-		Cliente c = new Cliente("Victor", 60000.0, 20000.0);
-		
-		System.out.println("Nome do cliente: " + c.obterNome());
-		System.out.println("Saldo + Limite do cliente: R$ " + c.checarSaldo());
-		
-		c.sacar(15000.0);
-		c.depositar(25000.0);
-		
-		System.out.println("Saldo + Limite após operações: R$ " + c.checarSaldo());
-		
-		Transferencia transferencia = new Transferencia();
-		
-		Cliente c1 = new Cliente("João", 1000.0, 500.0);
-        Cliente c2 = new Cliente("Maria", 1500.0, 300.0);
-        
-        System.out.println("Saldo + Limite de " + c1.obterNome() + ": R$" + c1.checarSaldo());
-        System.out.println("Saldo + Limite de " + c2.obterNome() + ": R$" + c2.checarSaldo());
-
-        transferencia.transferir(c1, c2, 700.0);
-        System.out.println("Saldo + Limite de " + c1.obterNome() + " após transferência: R$" + c1.checarSaldo());
-        System.out.println("Saldo + Limite de " + c2.obterNome() + " após transferência: R$" + c2.checarSaldo());
-    
-
-	}
-
+    public static void main(String[] args) {
+        Telefone telefone1 = new Telefone("13", "9742-9999");
+        Cliente cliente1 = new Cliente("Carol", "123.456.789-00", telefone1);
+	    
+        cliente1.mostrarDados();
+	    
+        cliente1.adicionarTelefone("11", "8745-8888");
+	    
+        cliente1.mostrarDados();
+    }
 }
