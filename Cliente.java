6b1a8c5813e878bfe362e6222ba94d45345b00b4
Coraville <108678949+Coraville@users.noreply.github.com>
2024-03-22 17:36:44 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,46 +1,21 @@
-package lista;
-
 public class Cliente {
-	
-	private String nome;
-	private double saldo, limite;
-	
-	public Cliente(String nome, double saldo, double limite) {
-		
-		this.nome = nome;
-		this.saldo = saldo;
-		this.limite = limite;
-	}
-	
-	public void sacar(double valor) {
-		
-		if(saldo + limite >= valor) {
-			saldo -= valor;
-			System.out.println("Saque de R$ " + valor + " realizado com sucesso.");
-		}
-		else {
-			System.out.println("Saldo insulficiente!");
-		}
-		
-	}
-	
-	public void depositar(double valor) {
-		
-		saldo += valor;
-		System.out.println("Deposito no valor de R$ " + valor + " efetuado com sucesso.");
-		
-	}
-	
-	public double checarSaldo(){
-		
-		return saldo + limite;
-		
-	}
-	
-	public String obterNome() {
-		
-		return nome;
-		
-	}
-
+    private String nome;
+    private String cpf;
+    private Telefone telefone;
+    
+    public Cliente(String nome, String cpf, Telefone telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+    
+    public void mostrarDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Telefone: (" + telefone.obterDDD() + ") " + telefone.obterNumero());
+    }
+    
+    public void adicionarTelefone(String novoDdd, String novoNumero) {
+        telefone = new Telefone(novoDdd, novoNumero);
+    }
 }
